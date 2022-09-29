74e44c72cd2f3f2461dddce771d95a8892e68025
Aarik Pokras <85502246+aarikpokras@users.noreply.github.com>
2022-09-29 16:46:14 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+var engi = document.getElementById("engines");
+function engOneOn() {
+	engi.innerHTML = "ENG 1 0% | ENG 2 OFF";
+}
+function engTwoOn() {
+  engi.innerHTML = "ENG 1 0% | ENG 2 0%";
+}
